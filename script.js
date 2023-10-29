9b82e01f3e4bfca7dd691111c7f446e03d30bbac
Tokioshy <grizzlizyping@gmail.com>
2023-10-29 13:52:45 +0700

    latest

--- script.js
+++ script.js
@@ -19,3 +19,12 @@ function topFunction() {
     document.body.scrollTop = 0; // For Safari
     document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+window.onload=function() {
+    var mobile = (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(navigator.userAgent.toLowerCase()));
+    if (mobile) {
+        alert("Visit this on a Computer for Better View or Change to Dekstop Mode")
+    } else {
+        
+    }
+}
